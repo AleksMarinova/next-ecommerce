322dfd19eec8e4a148aa6ea1ada8f6bad2ffca5d
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-15 11:18:15 +0100

    added remove item and clear cart

--- pages/cart.js
+++ pages/cart.js
@@ -2,6 +2,9 @@ import useStore from "../zustand/store";
 
 const Cart = () => {
   const { cartItems } = useStore(state => state);
+  const removeFromCart = useStore(state => state.removeFromCart);
+  const clearCart = useStore(state => state.clearCart);
+
   return (
     <div>
       <h1>Cart</h1>
@@ -9,9 +12,20 @@ const Cart = () => {
         {cartItems.map(item => (
           <li key={item.id}>
             {item.title} - {item.price}
+            <button onClick={(e)=> {
+              e.stopPropagation();
+              e.preventDefault();
+              removeFromCart(item);
+            }} >Remove</button>
           </li>
         ))}
       </ul>
+      <button>Checkout</button>
+      <button onClick={(e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+        clearCart();
+      }}>Clear cart</button>
     </div>
   )
 }
--- zustand/store.js
+++ zustand/store.js
@@ -3,6 +3,8 @@ import create from 'zustand';
 const useStore = create(set => ({
   cartItems: [],
   addToCart: (item) => set(state => ({ cartItems: [...state.cartItems, item] })),
+  removeFromCart: (item) => set(state => ({ cartItems: state.cartItems.filter(i => i.id !== item.id) })),
+  clearCart: (item) => set(state => ({ cartItems: [] })),
 }));
 
 export default useStore;
